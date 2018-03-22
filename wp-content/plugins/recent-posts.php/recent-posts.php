1b8080c3af2616ef121e3faad05c54882523d9e3
Andre Mander <andre.mander@gmail.com>
2018-03-21 20:03:36 -0400

    started recent posts widget

---
+++ wp-content/plugins/recent-posts.php/recent-posts.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: Recent Posts
+Plugin URI: http://andremander.com
+Version: 1.0
+Author: Andre Mander
+Description: This plugin will feature the last 3 posts, sorted by category.
+*/
